ebecc98d7b6d52deee558bac886ab60db898167c
Yesid Marin <yesid@autentikar.com>
2025-04-11 20:32:14 -0500

    version 1.0.0

---
+++ AutentikarUnifiedID.xcframework/ios-arm64/AutentikarUnifiedID.framework/Headers/AutentikarUnifiedID.h
@@ -0,0 +1,18 @@
+//
+//  AutentikarUnifiedID.h
+//  AutentikarUnifiedID
+//
+//  Created by Yesid Marin on 2/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AutentikarUnifiedID.
+FOUNDATION_EXPORT double AutentikarUnifiedIDVersionNumber;
+
+//! Project version string for AutentikarUnifiedID.
+FOUNDATION_EXPORT const unsigned char AutentikarUnifiedIDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutentikarUnifiedID/PublicHeader.h>
+
+
---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AutentikarUnifiedID",
+    products: [
+        .library(
+            name: "AutentikarUnifiedID",
+            targets: ["AutentikarUnifiedIDWrapper"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Autentikar/faceTec-dependency.git", from: "9.7.51"),
+    ],
+    targets: [
+        .target(
+            name: "AutentikarUnifiedIDWrapper",
+            dependencies: [
+                .product(name: "FaceTecSDK", package: "faceTec-dependency"),
+                .target(name: "AutentikarUnifiedID"),
+            ],
+            path: "AutentikarUnifiedIDWrapper"
+        ),
+        .binaryTarget(
+            name: "AutentikarUnifiedID",
+            path: "AutentikarUnifiedID.xcframework")
+    ]
+)
